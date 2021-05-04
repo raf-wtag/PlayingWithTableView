b861f793d6d6e44a99ea7e76a99ef0ab436860bd
raf-wtag <fahim.rahman@welldev.io>
2021-05-04 22:58:12 +0600

    Basic TableView Implemented

--- PlayingWithTableView/TableViewController.swift
+++ PlayingWithTableView/TableViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class TableViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataSource = ["User One","User Two","User Three","User Four","User Five","User Six","User Seven","User Eight","User Nine","User Ten"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
 
-
+extension TableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "table_cell")
+        cell?.textLabel?.text = dataSource[indexPath.row]
+        
+        return cell!
+    }
+    
+    
 }
 
